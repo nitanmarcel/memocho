966a8c88b9de3722cdb02459e1db019f1e6ac1fe
Marcel Alexandru Nitan <nitan.marcel@protonmail.com>
2021-05-27 17:08:18 +0000

    Add short param: if true redirects to the url in snippet

--- routes/getSnippet.js
+++ routes/getSnippet.js
@@ -2,27 +2,47 @@ const render = require('../render');
 
 module.exports = (req, res) => {
 
-	const { models, isHash } = req;
-	const [query, language] = (isHash ? req.params.hash : req.params.id || '').split('.');
-	const rawMode = Boolean(req.query.raw);
-	return (isHash ? models.snippets.findOne({ hash: query }) : models.snippets.findById(query))
-		.then(doc => {
-			if (!doc) {
-				res.status(404);
-				return res.send(
-					'<code>Nothing here, go <a href="/">back!</a></code>'
-				);
-			}
-			if (rawMode) {
-				res.writeHead(200, {"Content-Type": "text/plain; charset=utf-8"});
-				res.end(doc.content);
-			}
-			else
-				return render(
-					req.mithril,
-					req.render,
-					{ location: 'snippet', content: doc.content, language, }
-				).then(html => res.send(html));
-		});
+    const {
+        models,
+        isHash
+    } = req;
+    const [query, language] = (isHash ? req.params.hash : req.params.id || '').split('.');
+    const rawMode = Boolean(req.query.raw);
+    const shortURL = Boolean(req.query.short)
+    return (isHash ? models.snippets.findOne({
+            hash: query
+        }) : models.snippets.findById(query))
+        .then(doc => {
+            if (!doc) {
+                res.status(404);
+                return res.send(
+                    '<code>Nothing here, go <a href="/">back!</a></code>'
+                );
+            }
+            if (shortURL) {
+                let url;
+
+                try {
+                    url = new URL(doc.content.startsWith('http') ? doc.content : 'http://' + doc.content)
+                    res.writeHead(302, { 'Location': url.toString() });
+                    res.end();
+                    return;
+                } catch (_) {}
+            }
+            if (rawMode) {
+                res.writeHead(200, {
+                    "Content-Type": "text/plain; charset=utf-8"
+                });
+                res.end(doc.content);
+            } else
+                return render(
+                    req.mithril,
+                    req.render, {
+                        location: 'snippet',
+                        content: doc.content,
+                        language,
+                    }
+                ).then(html => res.send(html));
+        });
 
 };
